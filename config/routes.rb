c0eaf273a095c1c7d125b631407723171dcf7bee
OggiDanailov <ogidan@abv.bg>
2019-02-11 19:24:39 -0500

    Add coulmn name to the registration with devise; -Change the corresponding controller and overwriting the exisitng one wiht one customized

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
 root "pages#index"
+
+devise_for :users, :controllers => {:registrations => "registrations"}
 
 devise_scope :user do
   get 'login', to: 'devise/sessions#new'
